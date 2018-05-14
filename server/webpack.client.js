7fa508b8feae6f7e15fa7d3af480c22f1d7517fb
Ross Matthews <ross.matthews@ratedpeople.com>
2018-05-14 20:42:47 +0100

    Lessons 20 -24

--- server/webpack.client.js
+++ server/webpack.client.js
@@ -1,7 +1,8 @@
 const path = require('path');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base.js');
 
-module.exports = {
-
+const config = {
   // tell Webpack the root file of our client application
   entry: './src/client/client.js',
 
@@ -11,23 +12,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public')
-  },
-
-  // Tell webpack to run babel on every file it runs through
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          presets: [
-            'react',
-            'stage-0',
-            ['env', { targets: { browsers: ['last 2 versions'] }}]
-          ]
-        }
-      }
-    ]
   }
 }
+
+module.exports = merge(baseConfig, config);
