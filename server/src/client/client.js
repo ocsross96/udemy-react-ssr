120a7c75094c75607363eed0255331ef22d5a688
Ross Matthews <ross.matthews@ratedpeople.com>
2018-06-05 20:56:56 +0100

    Lessons 25 - 49

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -1,7 +1,23 @@
 // startup point for the client side application
+import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { renderRoutes } from 'react-router-config';
 
-import Home from './components/Home';
+import Routes from './Routes';
+import reducers from './reducers';
 
-ReactDOM.hydrate(<Home />, document.querySelector('#root'));
+const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+ReactDOM.hydrate(
+  <Provider store={store}>
+    <BrowserRouter>
+      <div>{renderRoutes(Routes)}</div>
+    </BrowserRouter> 
+  </Provider>,
+  document.querySelector('#root')
+);
--- server/src/helpers/renderer.js
+++ server/src/helpers/renderer.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { renderToString } from 'react-dom/server';
-import Home from '../client/components/Home';
+import { StaticRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { renderRoutes } from 'react-router-config';
+import Routes from '../client/Routes';
 
-export default () => {
-  const content = renderToString(<Home />);
+export default (req, store) => {
+  const content = renderToString(
+    <Provider store={store}>
+      <StaticRouter location={req.path} context={{}}>
+        <div>{renderRoutes(Routes)}</div>
+      </StaticRouter>
+    </Provider>
+  );
 
   return `
     <html>
